bc086ceef598cb00b4ef67871856db8531c847ee
Kumar P <161574943+kumar7505@users.noreply.github.com>
2025-03-08 22:40:20 +0530

    about and team

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.dirhome),
     path('locked', views.registration, name="login"),
     path('home', views.login, name="home"),
-    path('logout', views.logout, name="logout")
+    path('logout', views.logout, name="logout"),
+    path('about', views.about, name="about"),
+    path('team', views.team, name="team")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -7,14 +7,16 @@ from django.contrib import messages
 name = ''
 mail = ''
 
+
 def dirhome(req):
     return redirect('/locked')
+
 
 def check_user(req):
     global name, mail
     name = req.COOKIES.get('username')  # Get username from cookies
-    mail = req.COOKIES.get('email')  # Get email from cookies
-
+    mail = req.COOKIES.get('useremail')  # Get email from cookies
+    print(name, mail)
     if name and mail:
         return
     else:
@@ -31,11 +33,14 @@ def registration(req):
         name = req.POST.get('name')
         mail = req.POST.get('mail')
         if (name and mail and len(req.POST.get('password')) >= 8):
+            if not req.POST.get("terms"):
+                return render(req, 'login.html', {'active': 'register', 'error': 'Accept the terms to continue', 'name': name, 'mail': mail})
+
             if (User.objects.filter(Name=name).exists()):
-                return render(req, 'login.html', {'active': 'register', 'error': 'The UserName is already Taken'})
+                return render(req, 'login.html', {'active': 'register', 'error': 'The UserName is already Taken', 'name': name, 'mail': mail})
 
             if (User.objects.filter(Mail=mail).exists()):
-                return render(req, 'login.html', {'active': 'register', 'error': 'The UserEmail is already Taken'})
+                return render(req, 'login.html', {'active': 'register', 'error': 'The UserEmail is already Taken', 'name': name, 'mail': mail})
 
             data = User()
             data.Name = name
@@ -43,33 +48,39 @@ def registration(req):
             data.Password = make_password(req.POST.get('password'))
             data.save()
             return render(req, 'login.html', {'success': 'User Registartion was successful'})
+        elif (len(req.POST.get('password')) < 8):
+            return render(req, 'login.html', {'active': 'register', 'error': 'The Password must need to contain more than 8 words', 'name': name, 'mail': mail})
+
         print('Registered Failure')
     return render(req, 'login.html')
 
 
 def login(req):
-    if(req.COOKIES.get('username')):
+    if (req.COOKIES.get('username')):
         return render(req, 'home.html')
     if (req.method == "POST"):
         print("kumar")
         mail = req.POST.get('email')
-
         password = req.POST.get('password')
+        if (len(password) < 8):
+            return render(req, 'login.html', {'error': 'Passowrd need to havecd  more than 8 characters', 'umail': mail})
         data = User.objects.filter(Mail=mail).first()
         if data:
             if check_password(password, data.Password):
                 res = render(req, 'home.html', {
                     'username': data.Name,
-                    'email': data.Mail
+                    'email': data.Mail,
+                    'success': 'Login Successful'
                 })
 
                 res.set_cookie('username', data.Name, max_age=6 * 60 * 60)
                 res.set_cookie('useremail', data.Mail, max_age=6 * 60 * 60)
 
                 return res
-            return render(req, 'login.html', {'error': 'Passowrd was Incorrect'})
-        return render(req, 'login.html', {'error': 'UserMail was not registered'})
+            return render(req, 'login.html', {'error': 'Passowrd was Incorrect', 'umail': mail})
+        return render(req, 'login.html', {'error': 'UserEmail was not registered'})
     return render(req, 'login.html')
+
 
 def logout(req):
     global name, mail
@@ -80,3 +91,16 @@ def logout(req):
     res.delete_cookie('username')
     res.delete_cookie('useremail')
     return res
+
+
+def about(req):
+    if check_user:
+        print('kua')
+        return redirect('login')
+    return redirect('about')
+
+def team(req):
+    check_user(req)  # Ensure user is checked
+    if not req.COOKIES.get('username'):
+        return redirect('login')  # Redirect if the user is not logged in
+    return render(req, 'team.html')
